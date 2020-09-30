6653662336ae33110b6c6e800eb32e8f9434edc1
cheluvesha <cheluvesh.b@gmail.com>
2020-09-30 18:58:40 +0530

    [CHELUVESHA]Refactored : Made Object reference as Global

--- src/main/scala/com/generics/bridgelabz/Maximum3Variables.scala
+++ src/main/scala/com/generics/bridgelabz/Maximum3Variables.scala
@@ -1,33 +1,34 @@
 package com.generics.bridgelabz
 
 class Maximum3Variables {
-
+  val Result = 1
+  val StringResult = 0
   def maximumOf3NumbersInt(firstNumber:Int,secondNumber:Int,thirdNumber:Int): Int ={
     var maxNumber = firstNumber
-    if(secondNumber.compareTo(firstNumber) == 1 && secondNumber.compareTo(thirdNumber)== 1) {
+    if(secondNumber.compareTo(firstNumber) == Result && secondNumber.compareTo(thirdNumber) == Result) {
       maxNumber = secondNumber
     }
-    if(thirdNumber.compareTo(firstNumber) == 1 && thirdNumber.compareTo(secondNumber)== 1) {
+    if(thirdNumber.compareTo(firstNumber) == Result && thirdNumber.compareTo(secondNumber) == Result) {
       maxNumber = thirdNumber
     }
     maxNumber
   }
   def maximumOf3NumbersFloat(firstNumber:Float,secondNumber:Float,thirdNumber:Float): Float ={
     var maxNumber = firstNumber
-    if(secondNumber.compareTo(firstNumber) == 1 && secondNumber.compareTo(thirdNumber)== 1) {
+    if(secondNumber.compareTo(firstNumber) == Result && secondNumber.compareTo(thirdNumber) == Result) {
       maxNumber = secondNumber
     }
-    if(thirdNumber.compareTo(firstNumber) == 1 && thirdNumber.compareTo(secondNumber)== 1) {
+    if(thirdNumber.compareTo(firstNumber) == Result && thirdNumber.compareTo(secondNumber) == Result) {
       maxNumber = thirdNumber
     }
     maxNumber
   }
   def maximumOf3NumbersString(firstString:String,secondString:String,thirdString:String): String ={
     var maxString = firstString
-    if(secondString.compareTo(firstString) > 0 && secondString.compareTo(thirdString) > 0) {
+    if(secondString.compareTo(firstString) > StringResult && secondString.compareTo(thirdString) > StringResult) {
       maxString = secondString
     }
-    if(thirdString.compareTo(firstString) > 0 && thirdString.compareTo(secondString) > 0) {
+    if(thirdString.compareTo(firstString) > StringResult && thirdString.compareTo(secondString) > StringResult) {
       maxString = thirdString
     }
     maxString
