dfae030b555be4f46ec6544d5a9d9378f8be38d1
cheluvesha <cheluvesh.b@gmail.com>
2020-09-30 18:51:01 +0530

    [CHELUVESHA]Refactored : Made Object reference as Global

--- src/main/scala/com/generics/bridgelabz/Maximum3Variables.scala
+++ src/main/scala/com/generics/bridgelabz/Maximum3Variables.scala
@@ -1,13 +1,13 @@
 package com.generics.bridgelabz
 
 class Maximum3Variables {
-
+  val Result = 1
   def maximumOf3NumbersInt(firstNumber:Int,secondNumber:Int,thirdNumber:Int): Int ={
     var maxNumber = firstNumber
-    if(secondNumber.compareTo(firstNumber) == 1 && secondNumber.compareTo(thirdNumber)== 1) {
+    if(secondNumber.compareTo(firstNumber) == Result && secondNumber.compareTo(thirdNumber) == Result) {
       maxNumber = secondNumber
     }
-    if(thirdNumber.compareTo(firstNumber) == 1 && thirdNumber.compareTo(secondNumber)== 1) {
+    if(thirdNumber.compareTo(firstNumber) == Result && thirdNumber.compareTo(secondNumber) == Result) {
       maxNumber = thirdNumber
     }
     maxNumber
--- src/test/scala/com/genericsTest/bridgelabz/Maximum3VariablesTest.scala
+++ src/test/scala/com/genericsTest/bridgelabz/Maximum3VariablesTest.scala
@@ -4,19 +4,16 @@ import com.generics.bridgelabz.Maximum3Variables
 import org.scalatest.FunSuite
 
 class Maximum3VariablesTest extends FunSuite {
-
+  val maximum3Variables = new  Maximum3Variables()
   test("MaximumInputNumberAtFirstFollowedBySecondAndThirdInput_ReturnFirstInputAsMaximum"){
-    val maximum3Variables = new  Maximum3Variables()
     assert(maximum3Variables.maximumOf3NumbersInt(7,4,5) === 7)
   }
 
   test("MaximumInputNumberAtSecondFollowedByFirstAndThirdInput_ReturnSecondInputAsMaximum") {
-    val maximum3Variables = new  Maximum3Variables()
     assert(maximum3Variables.maximumOf3NumbersInt(5,7,6) === 7)
   }
 
   test("MaximumInputNumberAtThirdFollowedByFirstAndSecondInput_ReturnThirdInputAsMaximum") {
-    val maximum3Variables = new  Maximum3Variables()
     assert(maximum3Variables.maximumOf3NumbersInt(2,6,8) === 8)
   }
 
