1514860d6096f27fefff6e3015efbc28b3d44384
cheluvesha <cheluvesh.b@gmail.com>
2020-10-01 16:08:45 +0530

    [CHELUVESHA]Refactored : Variables name

--- src/main/scala/com/generics/bridgelabz/Maximum3Variables.scala
+++ src/main/scala/com/generics/bridgelabz/Maximum3Variables.scala
@@ -2,9 +2,9 @@ package com.generics.bridgelabz
 
 import Ordering.Implicits._
 class Maximum3Variables[T:Ordering](var type1:T, var type2:T, var type3:T){
-  val first = type1
-  val second = type2
-  val third = type3
+  val first: T = type1
+  val second: T = type2
+  val third: T = type3
 
   def this() {
     this(0.asInstanceOf[T],1.asInstanceOf[T],2.asInstanceOf[T])
@@ -18,5 +18,5 @@ class Maximum3Variables[T:Ordering](var type1:T, var type2:T, var type3:T){
     return max.asInstanceOf[T]
   }
 
-  def compare[T : Ordering](first: T, second: T) : T = if(first > second) first else second
+  def compare[T : Ordering](firstType: T, secondType: T) : T = if(firstType > secondType) firstType else secondType
 }
